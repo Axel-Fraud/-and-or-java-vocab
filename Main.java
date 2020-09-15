32dd3d1b30cb99511874889a9ab38c6d3542e62d
Axel Fraud <frauax1@gmail.com>
2020-09-15 23:34:30 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) {
+   int total = (int)(Math.random()* 100);
+   int minus = (int)(Math.random()* 100);
+   if(total >= 75 && minus >= 75){
+     System.out.println("You win a big prize");
+     }
+     else if(total >= 50 || minus >= 50){
+       System.out.println("You win a small prize");
+     }
+     else{ System.out.println("U loose");}
+     String num = Integer.toString(10);
+
+
+
+
+
+
+
+
+
+   }
+  }
